e5276d851be4aa4ac6a7acbf060521019977670a
Daniel Rodrigo Beltrame <danielrbeltrame@gmail.com>
2022-04-30 11:03:50 -0300

    Finalizando Contador

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
+import { useState } from "react"
+
  export default function App() {
-    return <h1>Oi</h1>
+
+    // let numero = 0
+    const [numero, setNumero] = useState(0)
+
+    function aumentar() {
+       setNumero(numero + 1)
+       console.log(numero)
+    }
+
+    function diminuir() {
+        setNumero(numero - 1)
+    }
+
+    return <section>
+        <h1>Contador</h1>
+        <p>{numero}</p>
+        <button onClick={diminuir}>menos</button>
+        <button onClick={aumentar}>mais</button>
+    </section>
 }
